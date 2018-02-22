05450ddcf0b0bd575a0628f01fac0b89aff8aa0f
Melania <melania.gallego.22@gmail.com>
2018-02-22 23:26:29 +0100

    Info Activity

--- app/src/main/java/com/example/ejercicio_final2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_final2/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.ejercicio_final2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent().setClass(
+                        MainActivity.this, MenuActivity.class);
+                        startActivity(mainIntent);
+
+                        finish();
+
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 }
